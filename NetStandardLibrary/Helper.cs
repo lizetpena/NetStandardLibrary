7464b46e09936c6d73f3fefded8b6ce39aba7b9f
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-18 02:47:00 -0700

    Use 64 bit integer (long or System.Int64) instead of 32 bit integer (int)

--- NetStandardLibrary/Helper.cs
+++ NetStandardLibrary/Helper.cs
@@ -5,8 +5,8 @@ namespace NetStandardLibrary
     public class Helper
     {
         public static DateTime GetDate() => DateTime.Now;
-        private int? number;
+        private long? number;
 
-        public int? Number { get => number; set => number = value; }
+        public long? Number { get => number; set => number = value; }
     }
 }
