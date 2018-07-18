d2a2b5805e94b79ee78ed0db1bc67d4f7f5be574
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-18 02:04:05 -0700

    Add .NET Standard dummy class Helper

---
+++ NetStandardLibrary/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetStandardLibrary
+{
+    public class Helper
+    {
+        public static DateTime GetDate() => DateTime.Now;
+        private int? number;
+
+        public int? Number { get => number; set => number = value; }
+    }
+}
