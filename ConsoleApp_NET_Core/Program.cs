b068d5fe6e1e00e40f7c3455828debd3f1eed4c8
Lizet Pena De Sola <lipenade@microsoft.com>
2018-07-18 02:25:07 -0700

    Add .NET Core Console Application

---
+++ ConsoleApp_NET_Core/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetStandardLibrary;
+using System;
+
+namespace ConsoleApp_NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //code illustrating new string concatenation
+
+            var today=Helper.GetDate();
+            Console.WriteLine($@"Today's date is {today}");
+
+            //code illustrating new way to invoke constructors/instantiate an object
+            var foo = new Helper
+            {
+                Number = 8
+            };
+
+            Console.WriteLine($@"The 'lucky' number is: {foo.Number}");
+
+            Console.ReadLine();
+        }
+    }
+}
